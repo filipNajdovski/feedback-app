f275dca2a270a5293aa8d514ba0509ac3965f6a0
filipNajdovski <filip-najdovski@live.com>
2024-01-01 23:40:48 +0100

    updated form along with a few components

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useState } from 'react'
+import RatingSelect from './RatingSelect'
+import Card from './shared/Card'
+import Button from './shared/Button'
+
+function FeedbackForm() {
+    const [text, setText] = useState('')
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [rating, setRating] = useState(10)
+    const [message, setMessage] = useState('')
+
+    const handleTextChange = (e) => {
+        if(text === '') {
+            setBtnDisabled(true)
+            setMessage(null)
+        } else if (text !== '' && text.trim().length <= 10 ) {
+            setBtnDisabled(true)
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null)
+            setBtnDisabled(false)
+        }
+
+        setText(e.target.value)
+    }
+
+  return (
+    <Card>
+        <form>
+            <h2>How would you rate your service?</h2>
+            <RatingSelect select={(rating) => setRating(rating)} />
+            <div className="input-group">
+                <input onChange={handleTextChange} 
+                type="text" 
+                placeholder='Write a review'
+                value={text} 
+                />
+                <Button type="submit" version='secondary' isDisabled={btnDisabled}>Send</Button>
+            </div>
+
+            {message && <div className='message'>{message}</div>}
+        </form>
+    </Card>
+  )
+}
+
+export default FeedbackForm
