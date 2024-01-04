e104aa422a1e0fadda15ef750ffaaa32442edb6a
filipNajdovski <filip-najdovski@live.com>
2024-01-04 15:02:28 +0100

    finish

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -4,7 +4,7 @@ import RatingSelect from './RatingSelect'
 import Card from './shared/Card'
 import Button from './shared/Button'
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
     const [text, setText] = useState('')
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [rating, setRating] = useState(10)
@@ -25,9 +25,23 @@ function FeedbackForm() {
         setText(e.target.value)
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if(text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating
+            }
+
+            handleAdd(newFeedback)
+
+            setText('')
+        }
+    }
+
   return (
     <Card>
-        <form>
+        <form onSubmit={handleSubmit}>
             <h2>How would you rate your service?</h2>
             <RatingSelect select={(rating) => setRating(rating)} />
             <div className="input-group">
