a3d8c99a16a0a981a91161079a80d18edba451f5
filipNajdovski <filip-najdovski@live.com>
2024-01-24 17:57:43 +0100

    finish 2.0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { v4 as uuidv4} from 'uuid'
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import Header from "./components/Header";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from './pages/AboutPage';
+import AboutIconLInk from './components/AboutIconLInk';
+import Card from './components/shared/Card';
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,22 +25,37 @@ function App() {
         }
     }
 
+    const HomePage = () => {
+        return(
+            <>
+            <FeedbackForm handleAdd={addFeedback}/>
+    
+            <FeedbackStats feedback={feedback} />
+    
+            <FeedbackList 
+            feedback={feedback}
+            handleDelete={deleteFeedback} 
+            />
+            </>
+        )
+    }
+
     return (
-        <>
+        <Router>
             <Header />
             <div className="container">
+                <Routes>
 
-                <FeedbackForm handleAdd={addFeedback}/>
+                    <Route exact path='/' element={<HomePage/>} />
 
-                <FeedbackStats feedback={feedback} />
-
-                <FeedbackList 
-                feedback={feedback}
-                handleDelete={deleteFeedback} 
-                />
+                
+                    <Route path='/about' element={<AboutPage/>} />                    
+                </Routes>
 
             </div>
-        </>
+
+            <AboutIconLInk />
+        </Router>
     )
 }
 
